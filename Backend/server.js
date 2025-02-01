e191075dc44b7753b57ef00ecad0bea8adcf83c6
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2025-02-01 20:33:16 +0530

    Added the add product API

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js'
+import Product from './models/product.js';
 
 // Load environmental variables
 dotenv.config();
 
 // express() initializes an Express application and assigns it to app
 const app = express();
+
+// Allows to accept JSON data in the req.body
+app.use(express.json());
 
 /*
 * Sets up a HTTP route at /
@@ -16,6 +20,25 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Server ready");
 })
+
+// API to create a new product
+app.post("/api/add-product", async (req, res) => {
+    const product = req.body; // user will send this data
+    
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all the required fields" });
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch(error) {
+        console.error("Error in creating product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 
 /*
 * Start the server
